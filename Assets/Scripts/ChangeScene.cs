3540ce89cb02e6f1039cdd949968d026a3c86e32
huscongao1692003 <look1692003@gmail.com>
2024-03-12 22:18:44 +0700

    FirstCommit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+ public class ChangeScene : MonoBehaviour
+ {
+  
+
+    public void startBtn()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+     //Output the new state of the Toggle into Text
+   
+ }
